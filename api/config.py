4e48e68024d286742f806911d902a8e5da600039
EinarSalbouvet <esalo@equinor.com>
2025-03-26 09:53:17 +0100

    refactor: add DATABASE_ADDRESS as env variable

--- api/config.py
+++ api/config.py
@@ -2,27 +2,19 @@ import os
 from pydantic import AnyHttpUrl, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-class Settings(BaseSettings):
+class Settings(BaseSettings): 
+    default_database_address: str = 'database:8182'
     APP_ENVIRONMENT: str = os.getenv('APP_ENVIRONMENT', 'local')
     DB_PRIMARY_KEY: str = os.getenv('DB_PRIMARY_KEY', 'key')
+    DATABASE_ADDRESS: str = os.getenv('DATABASE_ADDRESS', default_database_address)
 
     # frontend
     REACT_APP_WEB_HOST: str = "localhost"
     REACT_APP_WEB_PORT: str = "3000"
-    COSMOS_DB_NAME: str = "decisionDB"
-    COSMOS_CONTAINER: str = "decisionItems"
 
-    @computed_field
-    def DATABASE_HOST(self) -> str:
-        if self.APP_ENVIRONMENT.lower().__contains__('local'):
-            return 'database'
-        return os.getenv('COSMOS_ACCOUNT_URL', '')
-    
-    @computed_field
-    def DATABASE_PORT(self) -> str:
-        if self.APP_ENVIRONMENT.lower().__contains__('local'):
-            return '8182'
-        return os.getenv('COSMOS_PORT', '')
+    # static cosmos parameters
+    COSMOS_DB_NAME: str = "decisionDB"
+    COSMOS_CONTAINER: str = "prototype"
 
     @computed_field
     def FRONTEND_URL(self) -> AnyHttpUrl:
@@ -31,15 +23,15 @@ class Settings(BaseSettings):
 
     @computed_field
     def DATABASE_URL(self) -> AnyHttpUrl:
-        return f"http://{self.DATABASE_HOST}:{self.DATABASE_PORT}"
+        return f"http://{self.DATABASE_ADDRESS}"
 
     @computed_field
     def DATABASE_CONNECTION(self) -> str:
         
-        if self.DATABASE_HOST == "database":
-            return f"ws://{self.DATABASE_HOST}:{self.DATABASE_PORT}/gremlin"
+        if self.DATABASE_ADDRESS == self.default_database_address:
+            return f"ws://{self.DATABASE_ADDRESS}/gremlin"
         else:
-            return f"wss://{self.DATABASE_HOST}:{self.DATABASE_PORT}/gremlin"
+            return f"wss://{self.DATABASE_ADDRESS}/gremlin"
 
     # backend
     @computed_field
